52d204bf82e0c5630afecdc1be910f426e954449
lemon-ghost <iniesabado@gmail.com>
2022-11-04 13:45:36 +0800

    error fix

--- resources/views/members/show.blade.php
+++ resources/views/members/show.blade.php
@@ -22,7 +22,6 @@ tr, th, td,{
     /* background-color:white; */
     box-shadow:none;
 }
-
 table.dataTable thead th {
     border: none;
 }
@@ -76,7 +75,6 @@ table.dataTable tbody tr {
                                 <th>Membership End</th>
                                 <th></th>
                                 <th></th>
-                                <th></th>
                             </tr>
                         </thead>
                         <tbody>
@@ -102,21 +100,15 @@ table.dataTable tbody tr {
                                 </a>
                                 </td>
                                 <td class="deleteTD">
-                                {!! Form::open(['action' => ['CustomerController@destroy', $customer->id],'method'=>'POST','class' => '']) !!}
-                                
-                                    {{Form::hidden('_method','DELETE')}}
-                                    <!-- {{Form::submit('Delete', ['class'=> ''])}} -->
-                            
                                     <label class="removeInput">
-                                                    <svg class="icons" xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-trash" viewBox="0 0 16 16">
-                                                    <path d="M5.5 5.5A.5.5 0 0 1 6 6v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5zm2.5 0a.5.5 0 0 1 .5.5v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5zm3 .5a.5.5 0 0 0-1 0v6a.5.5 0 0 0 1 0V6z"/>
-                                                        <path fill-rule="evenodd" d="M14.5 3a1 1 0 0 1-1 1H13v9a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V4h-.5a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1H6a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1h3.5a1 1 0 0 1 1 1v1zM4.118 4 4 4.059V13a1 1 0 0 0 1 1h6a1 1 0 0 0 1-1V4.059L11.882 4H4.118zM2.5 3V2h11v1h-11z"/>
-                                                    </svg>
-                                                    <input class="hideInput" type="submit" name="delete">
-                                    </label>                  
-                                {!! Form::close() !!}
-                                </td>
                                 
+                                        <svg class="icons" xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-trash" viewBox="0 0 16 16">
+                                        <path d="M5.5 5.5A.5.5 0 0 1 6 6v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5zm2.5 0a.5.5 0 0 1 .5.5v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5zm3 .5a.5.5 0 0 0-1 0v6a.5.5 0 0 0 1 0V6z"/>
+                                            <path fill-rule="evenodd" d="M14.5 3a1 1 0 0 1-1 1H13v9a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V4h-.5a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1H6a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1h3.5a1 1 0 0 1 1 1v1zM4.118 4 4 4.059V13a1 1 0 0 0 1 1h6a1 1 0 0 0 1-1V4.059L11.882 4H4.118zM2.5 3V2h11v1h-11z"/>
+                                        </svg>
+                                        <input class="hideInput deleteUserBtn" type="" name="delete" value="{{$customer->id}}">
+                                    </label>
+                                </td>
                             </tr>
                         </tbody>
                         <div class="modal fade" id="deleteModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
@@ -141,6 +133,7 @@ table.dataTable tbody tr {
                             </div>
                         </div>
                 </table>
+ 
                 <div class="table-div">
                     <div class="my-custom-row d-flex flex-row justify-content-between " >
                         <div class="col">
@@ -148,13 +141,8 @@ table.dataTable tbody tr {
                         </div>
                         
                         <a href="/members/{{$customer->id}}/class/create">
-                            <button class="enrollBtn">
+                            <button class="enrollBtn btn btn-primary">
                             Enroll?
-                            <svg class="icons" xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-journal-arrow-up" viewBox="0 0 16 16">
-                                <path fill-rule="evenodd" d="M8 11a.5.5 0 0 0 .5-.5V6.707l1.146 1.147a.5.5 0 0 0 .708-.708l-2-2a.5.5 0 0 0-.708 0l-2 2a.5.5 0 1 0 .708.708L7.5 6.707V10.5a.5.5 0 0 0 .5.5z"/>
-                                <path d="M3 0h10a2 2 0 0 1 2 2v12a2 2 0 0 1-2 2H3a2 2 0 0 1-2-2v-1h1v1a1 1 0 0 0 1 1h10a1 1 0 0 0 1-1V2a1 1 0 0 0-1-1H3a1 1 0 0 0-1 1v1H1V2a2 2 0 0 1 2-2z"/>
-                                <path d="M1 5v-.5a.5.5 0 0 1 1 0V5h.5a.5.5 0 0 1 0 1h-2a.5.5 0 0 1 0-1H1zm0 3v-.5a.5.5 0 0 1 1 0V8h.5a.5.5 0 0 1 0 1h-2a.5.5 0 0 1 0-1H1zm0 3v-.5a.5.5 0 0 1 1 0v.5h.5a.5.5 0 0 1 0 1h-2a.5.5 0 0 1 0-1H1z"/>
-                            </svg>
                             </button>
                         </a>
                 
@@ -259,7 +247,6 @@ table.dataTable tbody tr {
                         "emptyTable": "Costumer does not exist."
                     }
                 })
-
                 $('#second-table').DataTable({
                     "bInfo": false, //Dont display info e.g. "Showing 1 to 4 of 4 entries"
                     "paging": false,//Dont want paging                
@@ -270,7 +257,6 @@ table.dataTable tbody tr {
                         "emptyTable": "No Enrolled Classes"
                     }
                 })   
-
         </script>
 
 @endsection
@@ -282,13 +268,10 @@ table.dataTable tbody tr {
             $(document).ready(function(){
                 $('.deleteUserBtn').click(function(e){
                     e.preventDefault();
-
                     var customer_id = $(this).val();
                     $('#customer_id').val(customer_id);
-
                     $('#deleteModal').modal('show');
                 });
-
             });
             $(document).ready(function(){
                 $('.unenrollUserBtn').click(function(e){
